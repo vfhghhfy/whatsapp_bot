ffb5b9ecadc08f5bf844d8eb2e921b4088aa6c15
vfhghhfy <tt8211099@gmail.com>
2025-09-26 16:55:57 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Kenny Bot',
   author: 'Wambugu Kinyua',
   botName: "Kinyua Bot",
-  botOwner: 'Wambugu Kinyua', // Your name
-  ownerNumber: '2547xxxxxx', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Dazai', // Your name
+  ownerNumber: '967778668253', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
